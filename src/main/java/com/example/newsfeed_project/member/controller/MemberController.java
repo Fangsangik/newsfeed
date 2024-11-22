6206c3bfff2943d7da702b09d66c32e311653dcb
Fangsangik <hwangsangik@gmail.com>
2024-11-22 21:36:51 +0900

    refactor : "MemberController" - pk 값으로 세션 유지

--- src/main/java/com/example/newsfeed_project/member/controller/MemberController.java
+++ src/main/java/com/example/newsfeed_project/member/controller/MemberController.java
@@ -1,17 +1,13 @@
 package com.example.newsfeed_project.member.controller;
 
-import com.example.newsfeed_project.exception.NoAuthorizedException;
 import com.example.newsfeed_project.member.dto.*;
 import com.example.newsfeed_project.member.service.MemberService;
-import com.example.newsfeed_project.util.SessionUtil;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.example.newsfeed_project.exception.ErrorCode.NO_AUTHOR_PROFILE;
 
 @RestController
 @RequestMapping("/members")
@@ -26,12 +22,13 @@ public class MemberController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getProfile(@PathVariable Long id) {
-        MemberDto memberByEmail = memberService.getMemberById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(memberByEmail);
+        MemberDto getMemberId = memberService.getMemberById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(getMemberId);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> createMember(@Valid @RequestBody MemberDto memberDto) {
+    public ResponseEntity<?> createMember(@Valid @RequestBody MemberDto memberDto, HttpSession session) {
+        Long loggedInUserId = (Long) session.getAttribute("id");
         MemberDto createdMember = memberService.createMember(memberDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMember);
     }
@@ -39,50 +36,27 @@ public class MemberController {
     @PutMapping("/update")
     public ResponseEntity<MemberUpdateResponseDto> updateMember(
             @Valid @RequestBody MemberUpdateRequestDto requestDto,
-            HttpServletRequest request) {
-        // 세션에서 이메일 추출
-        String email = SessionUtil.validateSession(request.getSession(false));
+            HttpSession session) {
+        Long loggedInUserId = (Long) session.getAttribute("id");
 
         // 업데이트 서비스 호출
-        MemberUpdateResponseDto responseDto = memberService.updateMember(email, requestDto);
+        MemberUpdateResponseDto responseDto = memberService.updateMember(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
-    @GetMapping("/email")
-    public ResponseEntity<?> findByEmail(@Valid @RequestParam String email, HttpServletRequest request) {
-        String sessionEmail = SessionUtil.validateSession(request.getSession(false));
-
-        if (!sessionEmail.equals(email)) {
-            throw new NoAuthorizedException(NO_AUTHOR_PROFILE);
-        }
-
-        MemberDto memberByEmail = memberService.getMemberByEmail(email);
-        return ResponseEntity.status(HttpStatus.OK).body(memberByEmail);
-    }
-
     @PutMapping("/password")
     public ResponseEntity<?> changePassword(@Valid @RequestBody PasswordRequestDto passwordRequestDto, HttpSession session) {
+        Long loggedInUserId = (Long) session.getAttribute("id");
         MemberDto memberDto = memberService.changePassword(passwordRequestDto.getOldPassword(), passwordRequestDto.getNewPassword(), session);
         return ResponseEntity.status(HttpStatus.OK).body(memberDto);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteMemberById(@Valid @RequestBody DeleteRequestDto deleteRequestDto, HttpServletRequest request) {
-        // 세션에서 이메일 확인
-        String email = SessionUtil.validateSession(request.getSession(false));
-        MemberDto existingMember = memberService.getMemberByEmail(email);
-
+    public ResponseEntity<?> deleteMemberById(@Valid @RequestBody DeleteRequestDto deleteRequestDto, HttpSession session) {
+        Long loggedInUserId = (Long) session.getAttribute("id");
         // 회원 탈퇴 처리 (비밀번호 검증 포함)
-        memberService.deleteMemberById(existingMember.getId(), deleteRequestDto.getPassword());
-
-
+        memberService.deleteMemberById(deleteRequestDto.getId(), deleteRequestDto.getPassword());
         return ResponseEntity.status(HttpStatus.OK).body("회원 삭제가 완료되었습니다.");
-    }
-
-    @PatchMapping("/{id}/restore")
-    public ResponseEntity<String> restoreMember(@PathVariable Long id) {
-        memberService.restoreMember(id);
-        return ResponseEntity.ok("회원이 복구되었습니다.");
     }
 }
